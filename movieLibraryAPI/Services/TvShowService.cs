05e0f852cd995a65304938b7d71ebb51862319d9
pisani <a.b.lindgard@gmail.com>
2026-05-07 12:15:55 +0200

    still working on making things work

--- movieLibraryAPI/Services/TvShowService.cs
+++ movieLibraryAPI/Services/TvShowService.cs
@@ -25,7 +25,6 @@ public class TvShowService
                 Title = input,
                 ReleaseYear = input.Length > 5 ? int.Parse(input.Substring(0, 4)) : 2000, // Just a placeholder for release year
                 Description = input,
-                Season = input.Length > 3 ? int.Parse(input.Substring(0, 2)) - 200 : 1, // Placeholder for season based on input length
                 TotalEpisodes = input.Length > 5 ? int.Parse(input.Substring(0, 4)) - 2000 + 10 : 10, // Placeholder for total episodes
             });
         }
@@ -36,11 +35,14 @@ public class TvShowService
             Title = "The X-Files",
             ReleaseYear = 1993,
             Description = "A pair of FBI agents investigate paranormal phenomena and unsolved cases, often involving extraterrestrial life.",
-            Season = 1,
             TotalEpisodes = 24,
-            EpisodeName = "Pilot",
-            EpisodeNumber = 01,
             TvShowGenre = Genres.SciFi
+        }, new TvShowEpisode
+        {
+            EpisodeName = "Pilot",
+            EpisodeNumber = 1,
+            SeasonNumber = 1,
+            Description = "FBI agents Mulder and Scully investigate their first case together, involving a series of mysterious deaths in a small town."
         });
     }
 }
