eb4635f31495f7f581d49e0144c82d853783cd23
pisani <a.b.lindgard@gmail.com>
2026-05-07 19:36:11 +0200

    started work on the next service-file

---
+++ movieLibraryAPI/Services/MovieService.cs
@@ -0,0 +1,14 @@
+using movieLibrary.Models;
+using movieLibrary.Models.TvShows;
+
+namespace movieLibrary.Services;
+
+/*
+public static class MovieService
+{
+    public static List<MovieAndTvShowList>()
+    {
+        Shows = new TvShow();
+    Movies = new Movie();
+}
+}*/
