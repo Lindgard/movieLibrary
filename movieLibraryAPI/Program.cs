6cfa012fed59c5e8b47600dc8ae5d5e67e43a291
ALP <a.b.lindgard@gmail.com>
2026-05-08 11:43:00 +0200

    added singletons of service files

--- movieLibraryAPI/Program.cs
+++ movieLibraryAPI/Program.cs
@@ -1,3 +1,4 @@
+using movieLibrary.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +7,10 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSingleton<MovieService>();
+builder.Services.AddSingleton<TvShowService>();
 
 var app = builder.Build();
 
