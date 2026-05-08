579de0aa904ec32992c608e1789de7ae415165d1
ALP <a.b.lindgard@gmail.com>
2026-05-08 11:08:48 +0200

    changed success property to statuscode and removed redundant constructor

--- movieLibraryAPI/Models/ApiResponse.cs
+++ movieLibraryAPI/Models/ApiResponse.cs
@@ -1,21 +1,8 @@
 namespace movieLibrary.Models;
 
-public class ApiResponse<T>
+public class ApiResponse
 {
-    public bool Success { get; set; }
+    public int StatusCode { get; set; }
     public string Message { get; set; } = string.Empty;
-    public T? Data { get; set; }
-
-    /// <summary>
-    /// Initializes a new instance of the ApiResponse class with the specified success status, message, and optional data.
-    /// </summary>
-    /// <param name="success">Indicates whether the API request was successful.</param>
-    /// <param name="message">A message providing additional information about the API response.</param>
-    /// <param name="data">The data returned by the API, if any.</param>
-    public ApiResponse(bool success, string message, T? data = default)
-    {
-        Success = success;
-        Message = message;
-        Data = data;
-    }
+    public object? Data { get; set; }
 }
