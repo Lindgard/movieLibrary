b74c1fceaadcfcca09aba72526ea5f4a5f66ba19
ALP <a.b.lindgard@gmail.com>
2026-05-29 12:50:42 +0200

    added migrations for EF Core. Including dotnet-tools to make it easier to run the same tool-version

---
+++ movieLibraryAPI/Data/Migrations/20260529104651_InitialPostgres.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace movieLibraryAPI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovieAndTvShowLists",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieAndTvShowLists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ReleaseYear = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Director = table.Column<string>(type: "text", nullable: false),
+                    MovieGenre = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TvShows",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ReleaseYear = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    TotalEpisodes = table.Column<int>(type: "integer", nullable: false),
+                    Creator = table.Column<string>(type: "text", nullable: false),
+                    TvShowGenre = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TvShows", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MovieListMovies",
+                columns: table => new
+                {
+                    MovieAndTvShowListId = table.Column<Guid>(type: "uuid", nullable: false),
+                    MovieId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovieListMovies", x => new { x.MovieAndTvShowListId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_MovieListMovies_MovieAndTvShowLists_MovieAndTvShowListId",
+                        column: x => x.MovieAndTvShowListId,
+                        principalTable: "MovieAndTvShowLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovieListMovies_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seasons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SeasonNumber = table.Column<int>(type: "integer", nullable: false),
+                    TvShowId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seasons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Seasons_TvShows_TvShowId",
+                        column: x => x.TvShowId,
+                        principalTable: "TvShows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TvShowListShows",
+                columns: table => new
+                {
+                    MovieAndTvShowListId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TvShowId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TvShowListShows", x => new { x.MovieAndTvShowListId, x.TvShowId });
+                    table.ForeignKey(
+                        name: "FK_TvShowListShows_MovieAndTvShowLists_MovieAndTvShowListId",
+                        column: x => x.MovieAndTvShowListId,
+                        principalTable: "MovieAndTvShowLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TvShowListShows_TvShows_TvShowId",
+                        column: x => x.TvShowId,
+                        principalTable: "TvShows",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Episodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    EpisodeName = table.Column<string>(type: "text", nullable: false),
+                    EpisodeNumber = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    SeasonNumber = table.Column<int>(type: "integer", nullable: false),
+                    SeasonId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Episodes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Episodes_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Episodes_SeasonId",
+                table: "Episodes",
+                column: "SeasonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovieListMovies_MovieId",
+                table: "MovieListMovies",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seasons_TvShowId",
+                table: "Seasons",
+                column: "TvShowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TvShowListShows_TvShowId",
+                table: "TvShowListShows",
+                column: "TvShowId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Episodes");
+
+            migrationBuilder.DropTable(
+                name: "MovieListMovies");
+
+            migrationBuilder.DropTable(
+                name: "TvShowListShows");
+
+            migrationBuilder.DropTable(
+                name: "Seasons");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "MovieAndTvShowLists");
+
+            migrationBuilder.DropTable(
+                name: "TvShows");
+        }
+    }
+}
