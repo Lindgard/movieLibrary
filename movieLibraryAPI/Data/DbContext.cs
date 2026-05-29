1a440d20d37a5a4b07142af9344eb077d764fd2b
ALP <a.b.lindgard@gmail.com>
2026-05-29 11:53:04 +0200

    rewritten modelBuilders to align better with PostgreSQL

--- movieLibraryAPI/Data/DbContext.cs
+++ movieLibraryAPI/Data/DbContext.cs
@@ -11,52 +11,77 @@ public class MovieLibraryDbContext : DbContext
     public DbSet<Season> Seasons => Set<Season>();
     public DbSet<Episode> Episodes => Set<Episode>();
     public DbSet<MovieAndTvShowList> MovieAndTvShowLists => Set<MovieAndTvShowList>();
-    public string DbPath { get; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        options.UseNpgsql($"Host=localhost;Database=movielibrary;Username=postgres;Password=postgres");
+        if (!options.IsConfigured)
+        {
+            options.UseNpgsql($"Host=localhost;Port=5432;Database=movielibrary;Username=postgres;Password=postgres");
+        }
     }
 
-    /// <summary>
-    /// Initializes a new instance of the MovieLibraryDbContext class. 
-    /// This constructor takes DbContextOptions as a parameter, which allows you to configure the context with specific options such as the database provider and connection string.
-    /// The constructor also sets the DbPath property to the path of the local application data folder, where the SQLite database file will be stored. 
-    /// This is useful for development and testing purposes, as it allows the database to be easily accessed and managed on the local machine.
-    /// </summary>
-    /// <param name="options">The options to be used by the DbContext.</param>
     public MovieLibraryDbContext(DbContextOptions<MovieLibraryDbContext> options) : base(options)
     {
-        var folder = Environment.SpecialFolder.LocalApplicationData;
-        var path = Environment.GetFolderPath(folder);
-        DbPath = System.IO.Path.Join(path, "movielibrary.db");
     }
 
+
+
     /// <summary>
-    /// Configures the entity relationships and mappings for the MovieLibraryDbContext. This method is called by the Entity Framework when the model is being created.
-    /// It defines how the entities (Movie, TvShow, Season, Episode, MovieAndTvShowList) are related to each other and how they should be mapped to the database tables.
+    /// Configure the many-to-many relationships between MovieAndTvShowList and Movie, and between MovieAndTvShowList and TvShow.
     /// </summary>
-    /// <param name="modelBuilder">The ModelBuilder used to configure the entity mappings.</param>
+    /// <param name="modelBuilder"></param>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<MovieAndTvShowList>()
-            .HasMany(m => m.Movies)
-            .WithMany(t => t.MovieAndTvShowLists)
-            .UsingEntity(j => j.ToTable("MovieListMovies"));
+        base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<MovieAndTvShowList>()
-            .HasMany(t => t.Shows)
-            .WithMany(l => l.MovieAndTvShowLists)
-            .UsingEntity(j => j.ToTable("TvShowListTvShows"));
+            .HasMany(l => l.Movies)
+            .WithMany(t => t.MovieAndTvShowLists)
+            .UsingEntity<Dictionary<string, object>>(
+                "MovieListMovies",
+                right => right.HasOne<Movie>()
+                    .WithMany()
+                    .HasForeignKey("MovieId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                Left => Left.HasOne<MovieAndTvShowList>()
+                    .WithMany()
+                    .HasForeignKey("MovieAndTvShowListId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                join =>
+                {
+                    join.HasKey("MovieAndTvShowListId", "MovieId");
+                }
+            );
+
+        modelBuilder.Entity<MovieAndTvShowList>()
+            .HasMany(l => l.Shows)
+            .WithMany(s => s.MovieAndTvShowLists)
+            .UsingEntity<Dictionary<string, object>>(
+                "TvShowListShows",
+                right => right.HasOne<TvShow>()
+                    .WithMany()
+                    .HasForeignKey("TvShowId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                Left => Left.HasOne<MovieAndTvShowList>()
+                    .WithMany()
+                    .HasForeignKey("MovieAndTvShowListId")
+                    .OnDelete(DeleteBehavior.Cascade),
+                Join =>
+                {
+                    Join.HasKey("MovieAndTvShowListId", "TvShowId");
+                }
+            );
 
         modelBuilder.Entity<Season>()
             .HasMany(s => s.Episodes)
             .WithOne(e => e.Season)
-            .HasForeignKey(e => e.SeasonId);
+            .HasForeignKey(e => e.SeasonId)
+            .OnDelete(DeleteBehavior.Cascade);
 
         modelBuilder.Entity<TvShow>()
             .HasMany(t => t.Seasons)
             .WithOne(s => s.TvShow)
-            .HasForeignKey(s => s.TvShowId);
+            .HasForeignKey(s => s.TvShowId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
