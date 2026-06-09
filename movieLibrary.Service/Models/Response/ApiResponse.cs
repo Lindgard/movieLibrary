6a8b8fd126b597be6fe05ca20eeb8b4bae97c249
ALP <a.b.lindgard@gmail.com>
2026-06-09 10:39:08 +0200

    added a boolean success to the model for api responses

--- movieLibrary.Service/Models/Response/ApiResponse.cs
+++ movieLibrary.Service/Models/Response/ApiResponse.cs
@@ -3,6 +3,7 @@ namespace movieLibraryService.Models.Response;
 public class ApiResponse<T>
 {
     public int StatusCode { get; set; }
+    public bool Success { get; set; }
     public string Message { get; set; } = string.Empty;
     public T? Data { get; set; }
 }
